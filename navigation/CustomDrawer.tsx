3031f9e1c259be60e1381e9c817b5bd2802dcc85
Jesuloba <jesulobajohn@gmail.com>
2022-07-16 10:22:45 +0100

    added all assets

---
+++ navigation/CustomDrawer.tsx
@@ -0,0 +1,6 @@
+import { View, Text, Image, TouchableOpacity } from "react-native";
+import {
+	createDrawerNavigator,
+	DrawerContentScrollView,
+} from "@react-navigation/drawer";
+import { MainLayout } from "../screens";
